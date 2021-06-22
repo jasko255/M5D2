ede250eb3dfea316d889eac533ec1e1919569dc7
jasko255 <jasko255@gmail.com>
2021-06-22 15:57:43 +0200

    1

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-import { Express } from "express";
+import  express  from "express";
+
+const server = express()
+const port = 3001
+
+server.listen(port, () => {
+    console.log('server is running on port ' + port);
+})
