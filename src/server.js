84aabb6f37b63fcc859ade14632ef9f92d7242dd
jasko255 <jasko255@gmail.com>
2021-06-29 15:12:47 +0200

    1

--- src/server.js
+++ src/server.js
@@ -5,12 +5,22 @@ import authorsRouter from "./services/authors/index.js";
 import postsRouter from './services/blog/index.js'
 
 const server = express()
-const port = 3001
-
-
-server.use(cors())
 
 server.use(express.json())
+
+const PORT = process.env.PORT || 3001
+
+const whitelist = [process.env.FRONTEND_URL, process.env.FRONTEND_PROD_URL]
+
+server.use(cors())
+//     {origin: function(origin, callback){
+//     if(!origin || whitelist.indexOf(origin) !== -1){
+//         callback(null, true)
+//     }
+//     else{ callback(new Error('Not allowed by cors!'))
+
+//     }
+// }}))
 
 // ENDPOINTS
 
@@ -19,6 +29,6 @@ server.use('/blogPosts', postsRouter)
 
 console.table(listEndpoints(server))
 
-server.listen(port, () => {
-    console.log('server is running on port ' + port);
+server.listen(PORT, () => {
+    console.log('server is running on port ' + PORT);
 })
